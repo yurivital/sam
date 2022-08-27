2c1bf93c63697e00a3c36adcd678acf2d971cade
Yuri Vital <vital.systm@gmail.com>
2022-08-27 23:13:03 +0200

    First view for Entity, Project, Document hierarchy

--- sam/manager/urls.py
+++ sam/manager/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'manager'
 urlpatterns = [
     path('', views.index, name='index'),
+    path("entity/<int:entity_id>/", views.entityDetail, name="entity"),
+    path("entity/<int:entity_id>/project/<int:project_id>/", views.projectDetail, name="project")
 ]
