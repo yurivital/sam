554f31211443f16132b70c42818e452d6314a147
Yuri Vital <vital.systm@gmail.com>
2022-10-02 23:21:07 +0200

    Use Bootstrap 5 as css framework

--- sam/sam/settings.py
+++ sam/sam/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'manager.apps',
     'manager.apps.ManagerConfig',
+    'bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'sam.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
